021db9f403503ee50e13ce16b83b45b3d9f754fc
Glyphack <sh.hooshyari@gmail.com>
2023-09-10 14:40:34 +0200

    make visibility public

--- ruff_python_import_resolver/src/import_result.rs
+++ ruff_python_import_resolver/src/import_result.rs
@@ -9,30 +9,30 @@ use crate::py_typed::PyTypedInfo;
 #[allow(clippy::struct_excessive_bools)]
 pub struct ImportResult {
     /// Whether the import name was relative (e.g., ".foo").
-    pub(crate) is_relative: bool,
+    pub is_relative: bool,
 
     /// Whether the import was resolved to a file or module.
-    pub(crate) is_import_found: bool,
+    pub is_import_found: bool,
 
     /// The path was partially resolved, but the specific submodule
     /// defining the import was not found. For example, `foo.bar` was
     /// not found, but `foo` was found.
-    pub(crate) is_partly_resolved: bool,
+    pub is_partly_resolved: bool,
 
     /// The import refers to a namespace package (i.e., a folder without
     /// an `__init__.py[i]` file at the final level of resolution). By
     /// convention, we insert empty `PathBuf` segments into the resolved
     /// paths vector to indicate intermediary namespace packages.
-    pub(crate) is_namespace_package: bool,
+    pub is_namespace_package: bool,
 
     /// The final resolved directory contains an `__init__.py[i]` file.
-    pub(crate) is_init_file_present: bool,
+    pub is_init_file_present: bool,
 
     /// The import resolved to a stub (`.pyi`) file within a stub package.
-    pub(crate) is_stub_package: bool,
+    pub is_stub_package: bool,
 
     /// The import resolved to a built-in, local, or third-party module.
-    pub(crate) import_type: ImportType,
+    pub import_type: ImportType,
 
     /// A vector of resolved absolute paths for each file in the module
     /// name. Typically includes a sequence of `__init__.py` files, followed
@@ -42,29 +42,29 @@ pub struct ImportResult {
     ///
     /// For example, resolving `import foo.bar` might yield `./foo/__init__.py` and `./foo/bar.py`,
     /// or `./foo/__init__.py` and `./foo/bar/__init__.py`.
-    pub(crate) resolved_paths: Vec<PathBuf>,
+    pub resolved_paths: Vec<PathBuf>,
 
     /// The search path used to resolve the module.
-    pub(crate) search_path: Option<PathBuf>,
+    pub search_path: Option<PathBuf>,
 
     /// The resolved file is a type hint (i.e., a `.pyi` file), rather
     /// than a Python (`.py`) file.
-    pub(crate) is_stub_file: bool,
+    pub is_stub_file: bool,
 
     /// The resolved file is a native library.
-    pub(crate) is_native_lib: bool,
+    pub is_native_lib: bool,
 
     /// The resolved file is a hint hint (i.e., a `.pyi` file) from
     /// `typeshed` in the standard library.
-    pub(crate) is_stdlib_typeshed_file: bool,
+    pub is_stdlib_typeshed_file: bool,
 
     /// The resolved file is a hint hint (i.e., a `.pyi` file) from
     /// `typeshed` in third-party stubs.
-    pub(crate) is_third_party_typeshed_file: bool,
+    pub is_third_party_typeshed_file: bool,
 
     /// The resolved file is a type hint (i.e., a `.pyi` file) from
     /// the configured typing directory.
-    pub(crate) is_local_typings_file: bool,
+    pub is_local_typings_file: bool,
 
     /// A map from file to resolved path, for all implicitly imported
     /// modules that are part of a namespace package.
@@ -83,7 +83,7 @@ pub struct ImportResult {
     pub(crate) py_typed_info: Option<PyTypedInfo>,
 
     /// The directory of the package, if any.
-    pub(crate) package_directory: Option<PathBuf>,
+    pub package_directory: Option<PathBuf>,
 }
 
 impl ImportResult {
@@ -114,7 +114,7 @@ impl ImportResult {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub(crate) enum ImportType {
+pub enum ImportType {
     BuiltIn,
     ThirdParty,
     Local,
