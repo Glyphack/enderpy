e76519749facc3e1fc6086f00c9541b3855908aa
Glyphack <sh.hooshyari@gmail.com>
2024-08-15 18:33:17 +0200

    Fix end offsets in snapshots

--- parser/src/lexer/mod.rs
+++ parser/src/lexer/mod.rs
@@ -1086,10 +1086,10 @@ impl<'a> Lexer<'a> {
         }
         count
     }
+}
 
-    fn match_whitespace(c: char) -> bool {
-        c.is_whitespace() && c != '\n' && c != '\r'
-    }
+fn match_whitespace(c: char) -> bool {
+    c.is_whitespace() && c != '\n' && c != '\r'
 }
 
 #[cfg(test)]
