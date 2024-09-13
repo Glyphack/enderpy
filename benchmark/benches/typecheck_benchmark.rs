cba472022886063b63072bd1f8d9aee9ff931695
Glyphack <sh.hooshyari@gmail.com>
2024-09-13 14:35:51 +0200

    Add more benchmarks for type checker

--- benchmark/benches/typecheck_benchmark.rs
+++ benchmark/benches/typecheck_benchmark.rs
@@ -15,22 +15,26 @@ pub use codspeed_criterion_compat::*;
 
 pub fn benchmark_type_checker(c: &mut Criterion) {
     let mut group = c.benchmark_group("type_checker");
-    let path = "test_data/types_very_fast.py";
+    let paths = vec![
+        "test_data/types_very_fast.py",
+        "../typechecker/test_data/inputs/conformance_tests/annotations_coroutine.py",
+    ];
+    for path in paths {
+        group.bench_with_input(
+            BenchmarkId::from_parameter(path.to_string()),
+            &path,
+            |b, path| {
+                b.iter(|| {
+                    let builder = BuildManager::new(Settings::test_settings());
+                    let file_path = PathBuf::from(path);
+                    builder.build_one(&PathBuf::from("../../"), &file_path);
+                    builder.type_check(&file_path);
 
-    group.bench_with_input(
-        BenchmarkId::from_parameter(path.to_string()),
-        &path,
-        |b, path| {
-            b.iter(|| {
-                let builder = BuildManager::new(Settings::test_settings());
-                let file_path = PathBuf::from(path);
-                builder.build_one(&PathBuf::from("../../"), &file_path);
-                builder.type_check(&file_path);
-
-                0
-            });
-        },
-    );
+                    0
+                });
+            },
+        );
+    }
 }
 
 fn get_config() -> Criterion {
